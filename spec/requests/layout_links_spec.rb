3a75919bcacc2b9ea09c3850e79e6b5417dd406f
Giri Prashant <max.payne7891@gmail.com>
2012-02-19 18:57:12 +0530

    Code ready with all the links ready including sign up

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -21,4 +21,10 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
+
+  it "should have a sign up page at '/signup' " do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+
 end
